926de0ce988d93d91e3ccc068b23975845a5d5ef
stariyboh227 <silnopeta@gmail.com>
2024-11-30 14:24:02 +0600

    Update main.py

--- main.py
+++ main.py
@@ -202,3 +202,44 @@ COMMON_NOUNS = [
     "участие",
 ]
 
+max_wrong = len(HANGMAN) - 1
+
+word = choice(COMMON_NOUNS) 
+so_far = "_" * len(word)  
+wrong = 0  
+used = []  
+
+while wrong < max_wrong and so_far != word:
+    print(HANGMAN[wrong]) 
+    print("\nВы использовали следующие буквы:\n", used)
+    print("\nНа данный момент слово выглядит так:\n", so_far)
+
+    guess = input("\n\nВведите свое предположение: ")  
+
+    while guess in used:
+        print("Вы уже вводили букву", guess)  
+        guess = input("Введите свое предположение: ") 
+
+    used.append(guess)  
+
+    if guess in word: 
+        print("\nДа!", guess, "есть в слове!")
+        new = ""
+        for i in range(len(word)):  
+            if guess == word[i]:
+                new += guess
+            else:
+                new += so_far[i]
+        so_far = new
+
+    else:
+        print("\nИзвините, буквы \"" + guess + "\" нет в слове.")  
+        wrong += 1
+
+if wrong == max_wrong:  
+    print(HANGMAN[wrong])
+    print("\nТы лох!")
+else:
+    print("\nВы угадали слово!")
+
+print("\nЗагаданное слово было \"" + word + '\"')
